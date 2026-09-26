101344ce37c825601cbfceb4b72f8ed1e686de90
Vinushuya <vinumithra2413@gmail.com>
2026-09-26 20:26:34 +0530

    test jankins actomatic build

--- products/apps.py
+++ products/apps.py
@@ -3,3 +3,26 @@ from django.apps import AppConfig
 
 class ProductsConfig(AppConfig):
     name = 'products'
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    name = "products"
+
+    def ready(self):
+        from django.template.context import BaseContext, Context
+
+        def base_context_copy(self):
+            duplicate = self.__class__.__new__(self.__class__)
+            duplicate.__dict__.update(self.__dict__)
+            duplicate.dicts = self.dicts[:]
+            return duplicate
+
+        BaseContext.__copy__ = base_context_copy
+
+        def context_copy(self):
+            duplicate = BaseContext.__copy__(self)
+            duplicate.render_context = self.render_context.__copy__()
+            return duplicate
+
+        Context.__copy__ = context_copy
